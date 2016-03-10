da89e6ab442a29e82d6686a54e0d076cd9ec4803
alejandro hernandez <alejandro hernandez>
2016-03-09 23:57:23 -0600

    1

---
+++ Regional/app/src/main/java/itcr/ac/cr/regional/MainActivity.java
@@ -0,0 +1,25 @@
+package itcr.ac.cr.regional;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+/*
+Evan Blanco
+Alejandro Hernandez
+
+La internacionalización sirve para que nuestra app no sea solo para
+un público meta como nuestro país de hogar sino para también que abarque
+nuevos mercados a nivel mundial debido a que el app store es global y no solo
+las personas de Latinoamérica verán nuestra app en el store
+
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
